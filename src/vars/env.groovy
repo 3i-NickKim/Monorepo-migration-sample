e06a9f6c13929cd8663e962c214b9e2b293b7faf
ecsimsw <ecsimsw@gmail.com>
2022-09-09 00:36:21 +0900

    echo to println

--- src/vars/env.groovy
+++ src/vars/env.groovy
@@ -1,5 +1,21 @@
 package vars
 
-def register(){
-    return "hello"
-}
+def G_POD_TEMPLATE = """
+     api version: v1
+     kind: Pod
+     spec:
+       nodeSelector:
+         service: cicd-builders
+       tolerations:
+       - key: "builder"
+         operator: "Equal"
+         value: "true"
+         effect: "NoSchedule"
+       containers:
+       - name: pivo-build-img
+         image: 3ijunejoo/pivo-build-img:latest
+         command:
+         - sleep
+         args:
+         - infinity
+     """
