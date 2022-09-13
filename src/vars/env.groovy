55dc1ab3b9aa272e58d4ac416595c5337030515b
nickecsimsw <nick.kim@3i.ai>
2022-09-13 11:10:19 +0900

    add node pod

--- src/vars/env.groovy
+++ src/vars/env.groovy
@@ -22,39 +22,41 @@ def GLOBAL_POD_TEMPLATE() {
      """
 }
 
-def NODE_POD_TEMPLATE_TEST='''
-api version: v1
-kind: Pod
-spec:
-  nodeSelector:
-    service: cicd-builders
-  tolerations:
-  - key: "builder"
-    operator: "Equal"
-    value: "true"
-    effect: "NoSchedule"
-  containers:
-  - name: pivo-docker-build-image
-    image: 3ijunejoo/pivo-docker-build-image:latest
-    imagePullPolicy: Always
-    securityContext:
-      privileged: true
-    volumeMounts:
-    - mountPath: /var/run
-      name: docker-socket
-    - name: cicd-storage
-      mountPath: /root/.aws
-      readOnly: true
-    command:
-    - sleep
-    args:
-    - infinity
-  volumes:
-  - name: docker-socket
-    hostPath:
-      path: /var/run
-      type: Directory
-  - name: cicd-storage
-    persistentVolumeClaim:
-      claimName: cicd-aws-claim
-'''
+def NODE_POD_TEMPLATE_TEST() {
+     return '''
+     api version: v1
+     kind: Pod
+     spec:
+       nodeSelector:
+         service: cicd-builders
+       tolerations:
+       - key: "builder"
+         operator: "Equal"
+         value: "true"
+         effect: "NoSchedule"
+       containers:
+       - name: pivo-docker-build-image
+         image: 3ijunejoo/pivo-docker-build-image:latest
+         imagePullPolicy: Always
+         securityContext:
+           privileged: true
+         volumeMounts:
+         - mountPath: /var/run
+           name: docker-socket
+         - name: cicd-storage
+           mountPath: /root/.aws
+           readOnly: true
+         command:
+         - sleep
+         args:
+         - infinity
+       volumes:
+       - name: docker-socket
+         hostPath:
+           path: /var/run
+           type: Directory
+       - name: cicd-storage
+         persistentVolumeClaim:
+           claimName: cicd-aws-claim
+     '''
+}
